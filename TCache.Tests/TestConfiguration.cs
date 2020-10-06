fbe5ef4763487641d046fb9ae87712204b7d185d
greenygh0st <dale.myszewski@gmail.com>
2020-10-05 23:29:45 -0500

    Added env var for tests

---
+++ TCache.Tests/TestConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCache.Tests
+{
+    class TestConfiguration
+    {
+        public static readonly string ENVRedisUri = Environment.GetEnvironmentVariable("TEST_REDIS_URI");
+    }
+}
